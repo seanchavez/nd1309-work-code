b303817adb845cb3125e3674f36bc28e5204ecaf
Sean Chavez <realseanchavez@gmail.com>
2019-01-03 21:05:18 -0800

    fuck

--- Course_Blockchain_Data/Practice_LevelDB_And_Promises/app.js
+++ Course_Blockchain_Data/Practice_LevelDB_And_Promises/app.js
@@ -5,25 +5,33 @@ const LevelSandboxClass = require('./levelSandbox.js');
 const db = new LevelSandboxClass.LevelSandbox();
 
 // Creating Data
-(function theLoop (i) {
-	setTimeout(function () {
-		//Test Object
-      	let objAux = {id: i, data: `Data #: ${i}`};
-        db.addLevelDBData(i, JSON.stringify(objAux).toString()).then((result) => {
-        	if(!result) {
-              console.log("Error Adding data");
-            }else {
-              console.log(result);
-            }
-        }).catch((err) => { console.log(err); });
-      	i++;
-		if (i < 10) { 
-          theLoop(i) 
+(function theLoop(i) {
+  setTimeout(function() {
+    //Test Object
+    let objAux = { id: i, data: `Data #: ${i}` };
+    db.addLevelDBData(i, JSON.stringify(objAux).toString())
+      .then(result => {
+        if (!result) {
+          console.log('Error Adding data');
         } else {
-        	db.getBlocksCount().then((count) => {
-	           console.log(`The id for your next object is: ${count}`);
-            }).catch((err) => { console.log(err); });
+          console.log(result);
+          console.log('fuck');
         }
-	}, 5600);
-  })(0);
-
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    i++;
+    if (i < 10) {
+      theLoop(i);
+    } else {
+      db.getBlocksCount()
+        .then(count => {
+          console.log(`The id for your next object is: ${count}`);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }, 5600);
+})(0);
